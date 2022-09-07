dc9bc16323a5a7d0aa650a5d31454e2809f815f2
kibet <kmishmael@gmail.com>
2022-09-07 19:48:14 +0300

    table header bug fix

--- src/tools/components/table.ts
+++ src/tools/components/table.ts
@@ -2,25 +2,19 @@ export const table = (params: { data: { content: string[][], withHeadings: boole
   let tableHeader = '';
   let tableBody = '';
   let template = '';
+  let headerClass = (params.data.withHeadings) ? 'tc-table--heading' : '';
 
  
-  tableHeader = getHeader(params)
+  tableHeader = (params.data.withHeadings) ? getHeader(params) : ''
 
-  for (let i = 1; i < params.data.content.length; i++) {
-    tableBody += `<div class="tc-row">`;
-
-    params.data.content[i].forEach((body) => {
-      tableBody += `<div class="tc-cell">${body}</div>`;
-    });
-    tableBody += `</div>`;
-  }
+  tableBody = getBody(params)
 
   template = `
   <div class="ce-block">
     <div class="ce-block__content">
       <div class="cdx-block">
         <div class="tc-wrap">
-          <div class="tc-table">
+          <div class="tc-table ${headerClass}">
             ${tableHeader}
             ${tableBody}
           </div>
@@ -33,13 +27,29 @@ export const table = (params: { data: { content: string[][], withHeadings: boole
   return template;
 };
 
+function getBody(params: { data: { content: string[][], withHeadings: boolean } }){
+  let tableBody = ``
+  let index = (params.data.withHeadings) ? 1 : 0
+
+  for (let i = index; i < params.data.content.length; i++) {
+    tableBody += `<div class="tc-row">`;
+
+    params.data.content[i].forEach((body) => {
+      tableBody += `<div class="tc-cell">${body}</div>`;
+    });
+    tableBody += `</div>`;
+  }
+
+  return tableBody
+}
+
 function getHeader(params: { data: { content: string[][], withHeadings: boolean } }){
   let headerBody = ``
   let header = ``
 
   if(params.data.withHeadings){
-    params.data.content[0].forEach((header: string) => {
-      header += `<div class="tc-cell">${header}</div>`;
+    params.data.content[0].forEach((head) => {
+      header += `<div class="tc-cell" heading="Heading">${head}</div>`;
     });
 
     headerBody = `  
